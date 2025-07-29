fa5383bfb7ea1bb13df02c2a66b9c17fe0cf7039
Charles Reyes <charleskylereyes16@gmail.com>
2025-07-29 17:28:03 +0800

    Addded shopping_cart schema to init-db

--- init-db.sh
+++ init-db.sh
@@ -3,4 +3,5 @@ psql -U "root" -d "ecom-platform" <<EOF
 CREATE SCHEMA IF NOT EXISTS auth;
 CREATE SCHEMA IF NOT EXISTS product;
 CREATE SCHEMA IF NOT EXISTS orders;
+CREATE SCHEMA IF NOT EXISTS shopping_cart;
 EOF
