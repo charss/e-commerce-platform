cf882fcf0c379904022a2e26df129ceb919c5e77
Charles Reyes <charleskylereyes16@gmail.com>
2025-07-29 17:31:23 +0800

    Shopping cart service initial commit

---
+++ shopping-cart-svc/src/main/java/com/example/shopping_cart_svc/ShoppingCartSvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopping_cart_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartSvcApplication.class, args);
+	}
+
+}
---
+++ shopping-cart-svc/src/test/java/com/example/shopping_cart_svc/ShoppingCartSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopping_cart_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
